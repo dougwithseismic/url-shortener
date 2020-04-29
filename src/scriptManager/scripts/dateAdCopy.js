8ddddcc5d8a65c7727bb80c43d1415f85d479acf
Doug <doug@withseismic.com>
2020-04-29 19:58:46 +0200

    feat: Time sensitive access check

--- src/scriptManager/scripts/dateAdCopy.js
+++ src/scriptManager/scripts/dateAdCopy.js
@@ -1,10 +1,10 @@
 const options = {
-    id: 3,
-    name: 'CTR Magnet - Include the date in AdCopy',
-    version: '1.0.1',
-    platform: 'Google Ads',
-    type: 'Script', 
-    setupInstructions:`/* 
+  id: 3,
+  name: 'CTR Magnet - Include the date in AdCopy',
+  version: '1.0.1',
+  platform: 'Google Ads',
+  type: 'Script',
+  setupInstructions: `/* 
 
     Dynamically Insert Today's Date, Day, Month, Year etc into adcopy using {=Date. ...}
     
@@ -29,124 +29,142 @@ const options = {
     Codealong: https://www.twitch.tv/videos/599249260
     
     */`
-  }
-  
-  const getScriptContent = () => {
-    return `function main() {
+}
 
-        var offsetHour = 9  // EDIT THIS - SEE SETUP INSTRUCTIONS
+const getScriptContent = () => {
+  return `var realTimezone = 'Europe/Prague' 
+
+    function main() {
+      
+      
+        var checkTime = Utilities.formatDate(new Date(), AdWordsApp.currentAccount().getTimeZone(), "MMMM dd, yyyy HH:mm:ss Z")
+    
+        if (typeof realTimezone !== 'undefined') {     
+        checkTime = Utilities.formatDate(new Date(), realTimezone, "MMMM dd, yyyy HH:mm:ss Z")
+      }  
+    
+    
+    
+      Logger.log('Running CTR Magnet - Date Customizer')
+      Logger.log('---')
+      Logger.log('Time: %s (%s)',  AdWordsApp.currentAccount().getTimeZone(), checkTime)
+      Logger.log('Is this the correct time? If not, refer to the setup guide for a quick fix!')
+      Logger.log('---')
+    
+      
+      var dataFeed = grabDataSource('Date-newdate')
+      var dateOptions = calcDate(0)
+      
+      if (typeof offsetHour !== 'undefined') {     
+        dateOptions = calcDate(offsetHour)
+      }
         
-        var dataFeed = grabDataSource('Date-newdate')
-        var dateOptions = calcDate(offsetHour)
-         
+    
+      
+      if (dataFeed.items().get().totalNumEntities() === 0) {  
         
-        if (dataFeed.items().get().totalNumEntities() === 0) {  
-          
-          Logger.log('Creating a new item in empty data feed')
-          dataFeed.adCustomizerItemBuilder().withAttributeValues(dateOptions).build()   
-        } else {
-          
-          var currentDataFeed = dataFeed.items().get().next()
-          Logger.log('Updating Feed values from current date')  
-          currentDataFeed.setAttributeValues(dateOptions)
-          
-        }
+        Logger.log('Creating a new item in empty data feed')
+        dataFeed.adCustomizerItemBuilder().withAttributeValues(dateOptions).build()   
+      } else {
         
-        
-        Logger.log('Script Finished, Dates Updated')
+        var currentDataFeed = dataFeed.items().get().next()
+        Logger.log('Updating Feed values from current date')  
+        currentDataFeed.setAttributeValues(dateOptions)
         
       }
       
       
-      function calcDate(offset) {
-       
-        var now = new Date()
-       var days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
-       var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec' ]
-        
-        // We need to correct our time
-       
+      Logger.log('Script Finished, Dates Updated')
       
+    }
+    
+    
+    function calcDate(offset) {
+     
+      var now = new Date()
+     var days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
+     var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec' ]
+      
+      // We need to correct our time
+     
+    
+      var today = Utilities.formatDate(now, AdWordsApp.currentAccount().getTimeZone(), "MMMM dd, yyyy HH:mm:ss Z")
+      
+      if (typeof realTimezone !== 'undefined') {     
+        today = new Date(Utilities.formatDate(new Date(), realTimezone, "MMMM dd, yyyy HH:mm:ss Z"))
+      }  
         
-       Logger.log('Is this time correct? %s (%s)? If not, refer to the setup guide for a quick fix!',  AdWordsApp.currentAccount().getTimeZone(), Utilities.formatDate(now,  AdWordsApp.currentAccount().getTimeZone(), "HH:mm:ss"))
-        
-        var today = Utilities.formatDate(now, AdWordsApp.currentAccount().getTimeZone(), "MMMM dd, yyyy HH:mm:ss Z")
-        if (typeof timeCorrection == 'undefined') {     
-          today = new Date(now.setHours(now.getHours() + offset ))
-        }  
+      
+      return {
+        today: today.toLocaleDateString(), // Wednesday 22nd April 2020
+        day: days[today.getDay()], 
+        ordinal: getOrdinal(today.getDate()), // 1st, 2nd, 3rd.. 
+        month: months[today.getMonth()],
+        date: today.getDate().toString(), 
+        year: today.getFullYear().toString()
+      } 
+      
+    }
+    
+    function getOrdinal(date) {
+     
+    if (date > 3 && date < 21) return date + 'th'
+      
+      switch (date % 10)  
+      {
+        case 1:
+          return date + 'st'
           
-        
-        return {
-          today: today.toLocaleDateString(), // Wednesday 22nd April 2020
-          day: days[today.getDay()], 
-          ordinal: getOrdinal(today.getDate()), // 1st, 2nd, 3rd.. 
-          month: months[today.getMonth()],
-          date: today.getDate().toString(), 
-          year: today.getFullYear().toString()
-        } 
-        
-      }
-      
-      function getOrdinal(date) {
-       
-      if (date > 3 && date < 21) return date + 'th'
-        
-        switch (date % 10)  
-        {
-          case 1:
-            return date + 'st'
-            
-          case 2:
-            return date + 'nd'
-            
-          case 3:
-            return date + 'rd'
-            
-            
-          default:
-            return date + 'th'
-              
-        } 
-        
-      }
-      
-      
-      function grabDataSource(name) {
-       
-         var sources = AdWordsApp.adCustomizerSources().get()
-         
-         while (sources.hasNext()) {
-           var source = sources.next()
-           if (source.getName() === name) {
-            
-             Logger.log('Found Data Feed Source: %s', source.getName())
-             return source
+        case 2:
+          return date + 'nd'
           
-           }
-              
+        case 3:
+          return date + 'rd'
+          
+          
+        default:
+          return date + 'th'
+            
+      } 
+      
+    }
+    
+    
+    function grabDataSource(name) {
+     
+       var sources = AdWordsApp.adCustomizerSources().get()
+       
+       while (sources.hasNext()) {
+         var source = sources.next()
+         if (source.getName() === name) {
+          
+           Logger.log('Found Data Feed Source: %s', source.getName())
+           return source
+        
          }
-        
-        Logger.log('No source found: creating new source %s', name)
-        return AdWordsApp.newAdCustomizerSourceBuilder()
-        .withName(name)
-        .addAttributes({
-          today: 'text',
-          day: 'text',
-          ordinal: 'text',
-          month: 'text',
-          date: 'text',
-          year: 'text'
-        })
-        .build()
-        .getResult()
-        
+            
+       }
       
-      }
+      Logger.log('No source found: creating new source %s', name)
+      return AdWordsApp.newAdCustomizerSourceBuilder()
+      .withName(name)
+      .addAttributes({
+        today: 'text',
+        day: 'text',
+        ordinal: 'text',
+        month: 'text',
+        date: 'text',
+        year: 'text'
+      })
+      .build()
+      .getResult()
+      
+    
+    }    
       `
-  }
-  
-  module.exports = {
-    ...options,
-    getScriptContent
-  }
-  
+}
+
+module.exports = {
+  ...options,
+  getScriptContent
+}
