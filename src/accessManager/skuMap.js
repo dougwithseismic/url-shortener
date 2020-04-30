adba3214ad1ee65743cc114b7d8525934e1d7592
Doug <doug@withseismic.com>
2020-04-30 16:48:44 +0200

    Changed script - daypart

--- src/accessManager/skuMap.js
+++ src/accessManager/skuMap.js
@@ -1,7 +1,7 @@
 export const skuMap = {
   map: [
     { sku: '001LIFETIME', scripts: [ 1, 2, 3 ], duration: 9999 },
-    { sku: 'ADSCRIPT001FREEBIE', scripts: [ 3 ], duration: 5 },
+    { sku: 'ADSCRIPT001FREEBIE', scripts: [ 3 ], duration: 365 },
     { sku: 'IPOD2008PINK', scripts: [ 1, 2 ], duration: 30 }
   ]
 }
