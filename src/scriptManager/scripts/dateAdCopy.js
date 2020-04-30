f297529e526ea22548f9eae0cd3c44eeeb3527eb
Doug <doug@withseismic.com>
2020-04-30 16:58:29 +0200

    Changed script - daypart

--- src/scriptManager/scripts/dateAdCopy.js
+++ src/scriptManager/scripts/dateAdCopy.js
@@ -87,8 +87,6 @@ const getScriptContent = () => {
     function calcDate(offset) {
      
       var now = new Date()
-     var days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
-     var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec' ]
       
       // We need to correct our time
      
