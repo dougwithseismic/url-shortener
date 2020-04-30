becf38cd66246e28ba4a7a57117dcad4c4f726e6
Doug <doug@withseismic.com>
2020-04-30 08:13:22 +0200

    product: added Script 3

--- src/accessManager/skuMap.js
+++ src/accessManager/skuMap.js
@@ -1,6 +1,7 @@
 export const skuMap = {
   map: [
-    { sku: '001LIFETIME', scripts: [ 1, 2 ], duration: 9999 },
+    { sku: '001LIFETIME', scripts: [ 1, 2, 3 ], duration: 9999 },
+    { sku: 'ADSCRIPT001FREEBIE', scripts: [ 3 ], duration: 5 },
     { sku: 'IPOD2008PINK', scripts: [ 1, 2 ], duration: 30 }
   ]
 }
