693a8a1d4b4e5d89331a6304615cf2568d47fcd2
Doug <doug@withseismic.com>
2020-05-01 11:03:14 +0200

    date Script update

--- src/scriptManager/scripts/dateAdCopy.js
+++ src/scriptManager/scripts/dateAdCopy.js
@@ -22,7 +22,7 @@ const options = {
     A. By default, scripts run at America/Los_Angeles (Pacific time) - to make scripts run at your accounts local timezone, 
     this script grabs the timezone of the account running the script. If this account setting has been set up incorrectly, the timing used might be off by as much as 12 hours!
 
-    To fix, add the following line, where x is the correct timezone, as found in this link https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
+    To fix, change the line below, where x is the correct timezone, as found in this link https://en.wikipedia.org/wiki/List_of_tz_database_time_zones
     var realTimezone = 'x'
 
     (https://developers.google.com/google-ads/scripts/docs/features/dates)
@@ -35,32 +35,59 @@ const options = {
     
     */
    
-   var days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
-   var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec' ]`
+   `
 }
 
 const getScriptContent = () => {
-  return `var realTimezone = 'Europe/Prague' 
+  return `
+  var realTimezone 
+  // realTimezone = 'Europe/Prague' // Uncomment this line (remove the //) if you need to edit your timezone.
+
+  var days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday' ]
+  var months = [ 'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec' ]
+  
+        var checkTime
+        var timeZoneString 
 
     function runScript() {
 
-      // Bing check - 
-      if (AdWordsApp == 'undefined') {
-        AdWordsApp = BingAdsApp
-      }
-      
-      
-        var checkTime = Utilities.formatDate(new Date(), AdWordsApp.currentAccount().getTimeZone(), "MMMM dd, yyyy HH:mm:ss Z")
+if (Utilities.formatDate == 'undefined') {
+  // Bing still doesnt have a formatDate utility equivalent so lets load momentJs to fix it.
+  Logger.log('Bing Script Mode Loaded')
+  eval(UrlFetchApp.fetch('https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.10.6/moment.min.js').getContentText())
+  eval(
+    UrlFetchApp.fetch(
+      'https://cdnjs.cloudflare.com/ajax/libs/moment-timezone/0.5.23/moment-timezone-with-data-2012-2022.min.js'
+    ).getContentText()
+  )
+
+  checkTime = moment(new Date()).tz(AdsApp.currentAccount().getTimeZone()).format('MMMM dd, yyyy HH:mm:ss Z')
+
+  if (typeof realTimezone !== 'undefined') {
+    checkTime = moment(new Date()).tz(realTimezone).format('MMMM dd, yyyy HH:mm:ss Z')
+    timeZoneString = realTimezone
+  }
+
+} else {
+  checkTime = Utilities.formatDate(new Date(), AdsApp.currentAccount().getTimeZone(), 'MMMM dd, yyyy HH:mm:ss Z')
+  timeZoneString = AdsApp.currentAccount().getTimeZone()
+
+  if (typeof realTimezone !== 'undefined') {
+    checkTime = Utilities.formatDate(new Date(), realTimezone, 'MMMM dd, yyyy HH:mm:ss Z')
+    timeZoneString = realTimezone
+  }
+}
+        
+
+
     
-        if (typeof realTimezone !== 'undefined') {     
-        checkTime = Utilities.formatDate(new Date(), realTimezone, "MMMM dd, yyyy HH:mm:ss Z")
-      }  
-    
+
+
     
     
       Logger.log('Running CTR Magnet - Date Customizer')
       Logger.log('---')
-      Logger.log('Time: %s (%s)',  AdWordsApp.currentAccount().getTimeZone(), checkTime)
+      Logger.log('Time: %s (%s)',  timeZoneString, checkTime)
       Logger.log('Is this the correct time? If not, refer to the setup guide for a quick fix!')
       Logger.log('---')
     
@@ -68,11 +95,7 @@ const getScriptContent = () => {
       var dataFeed = grabDataSource('Date')
       var dateOptions = calcDate(0)
       
-      if (typeof offsetHour !== 'undefined') {     
-        dateOptions = calcDate(offsetHour)
-      }
-        
-    
+   
       
       if (dataFeed.items().get().totalNumEntities() === 0) {  
         
@@ -92,14 +115,13 @@ const getScriptContent = () => {
     }
     
     
-    function calcDate(offset) {
+    function calcDate() {
      
       var now = new Date()
       
-      // We need to correct our time
-     
-    
-      var today = Utilities.formatDate(now, AdWordsApp.currentAccount().getTimeZone(), "MMMM dd, yyyy HH:mm:ss Z")
+      // We need to correct our time.. 
+         
+      var today = new Date(Utilities.formatDate(now, AdsApp.currentAccount().getTimeZone(), "MMMM dd, yyyy HH:mm:ss Z"))
       
       if (typeof realTimezone !== 'undefined') {     
         today = new Date(Utilities.formatDate(new Date(), realTimezone, "MMMM dd, yyyy HH:mm:ss Z"))
@@ -143,7 +165,7 @@ const getScriptContent = () => {
     
     function grabDataSource(name) {
      
-       var sources = AdWordsApp.adCustomizerSources().get()
+       var sources = AdsApp.adCustomizerSources().get()
        
        while (sources.hasNext()) {
          var source = sources.next()
@@ -157,7 +179,7 @@ const getScriptContent = () => {
        }
       
       Logger.log('No source found: creating new source %s', name)
-      return AdWordsApp.newAdCustomizerSourceBuilder()
+      return AdsApp.newAdCustomizerSourceBuilder()
       .withName(name)
       .addAttributes({
         today: 'text',
@@ -172,6 +194,7 @@ const getScriptContent = () => {
       
     
     }
+    
     
     runScript()
       `
